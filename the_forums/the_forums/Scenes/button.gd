ba1bb627187915bb7c7f0d760fd1f1c721e06966
Jackie JK <thejkgameplays@gmail.com>
2025-12-10 07:44:11 +0000

    Now with a cool screen (and featuring Dante from the Devil May Cry series)

--- the_forums/the_forums/Scenes/button.gd
+++ the_forums/the_forums/Scenes/button.gd
@@ -1,6 +1,6 @@
 extends Button
 func _on_enter_pressed():
-	get_tree().change_scene_to_file("res://Scenes/monitor.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Monitor.tscn")
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/monitor.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Monitor.tscn")
