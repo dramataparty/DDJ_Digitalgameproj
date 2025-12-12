fe3f59748df451ab9fe36f8b2eac3d7b81f6361b
Jackie JK <thejkgameplays@gmail.com>
2025-12-12 04:06:17 +0000

    for some reason the general scene switcher does not work (????) i'm too sleepy for this

--- the_forums/the_forums/Scenes/general_win.gd
+++ the_forums/the_forums/Scenes/general_win.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 signal request_scene_change(new_scene_path: String)
 
-func go_to_next_scene():
-	emit_signal("request_scene_change", "res://Scenes/Win.tscn")
-
 func _on_next_button_win(next_scene_path: String) -> void:
-	go_to_next_scene()
+	print("AAAA")
+	emit_signal("request_scene_change", next_scene_path)
+
+
+func _on_receptor_activated(item_id: String) -> void:
+	emit_signal("request_scene_change", "res://Scenes/Sports/Sports.tscn")
--- the_forums/the_forums/scripts/next_button.gd
+++ the_forums/the_forums/scripts/next_button.gd
@@ -2,7 +2,9 @@ extends Button
 signal win(next_scene_path: String)
 
 func _on_enter_pressed():
+	print("AAAA")
 	emit_signal("win","res://Scenes/Sports/Sports.tscn")
 
 func _on_pressed() -> void:
+	print("AAAA")
 	emit_signal("win","res://Scenes/Sports/Sports.tscn")
